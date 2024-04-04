50b45d622468bf727f3d1a87a639e2dbe5c7157f
abhijeettrivedi1 <abhijeettrivedi3064@gmail.com>
2024-04-04 16:23:16 +0530

    .

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
   credentials:true,
-  origin:"http://localhost:5173",
+  origin:"*",
 }));
  mongoose.connect(process.env.mongo_uri)
  .then(()=>console.log('Connected to Mongo'))
